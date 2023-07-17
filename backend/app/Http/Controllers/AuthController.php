aaf65fbf7a856728d6c832b2f6a0d4a6502a695a
Nael Mostafa <naelnael53.nm@gmail.com>
2023-07-17 18:06:16 +0300

    Passport auth configuration - Create authController - Modified auth, app,  AuthService and kernal to use passport - Add namespace to RouteService to fix controllers not found

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    // register
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $user->save();
+        $token = $user->createToken('authToken')->accessToken;
+        return response()->json([
+            'token' => $token
+        ]);
+    }
+
+    // login
+    public function login(Request $request)
+    {
+
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string'
+        ]);
+
+        $credentials = request(['email', 'password']);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        $user = $request->user();
+        $token = $user->createToken('authToken')->accessToken;
+        return response()->json([
+            'token' => $token
+        ]);
+    }
+
+    // logout
+    public function logout()
+    {
+        if (!Auth::check()) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+        Auth::user()->token()->revoke();
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
+
+    public function user()
+    {
+        $user = Auth::user();
+        return response()->json([
+            'user' => $user
+        ]);
+    }
+}
--- backend/app/Providers/AuthServiceProvider.php
+++ backend/app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         //
+        'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
     /**
@@ -21,6 +23,9 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+
+        Passport::tokensExpireIn(now()->addDays(1));
+        Passport::refreshTokensExpireIn(now()->addDays(2));
     }
 }
