19a8b446cec6b2ebc5e86ed7347a056842ad37d3
Nael Mostafa <naelnael53.nm@gmail.com>
2023-07-18 17:29:29 +0300

    migrate to yarn and fix proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,11 +1,17 @@
 const proxy = require('http-proxy-middleware');
    
+// module.exports = function(app) {
+//   app.use(
+//     '/api',
+//     proxy({
+//       target: 'http://localhost:8000',
+//       changeOrigin: true,
+//     })
+//   );
+// };
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 module.exports = function(app) {
-  app.use(
-    '/api',
-    proxy({
-      target: 'http://localhost:8000',
-      changeOrigin: true,
-    })
-  );
+  app.use(createProxyMiddleware('/api', { target: 'http://localhost:8000' }));
 };
