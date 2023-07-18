23a17ecf6caebe56360302846d7c9e27b1ccdd55
Nael Mostafa <naelnael53.nm@gmail.com>
2023-07-18 23:56:24 +0300

    queue and mailable

---
+++ backend/app/Jobs/SendEmailJob.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\MyMailable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Mail;
+
+class SendEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $email;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($email)
+    {
+        //
+        $this->email = $email;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        //
+        Mail::to($this->email)->send(new MyMailable());
+
+    }
+}
---
+++ backend/resources/views/mails/my-mailable.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Email Sent from laravel</title>
+</head>
+
+<body>
+    <p>Hello,</p>
+
+    <p>This is a test mail. This mail send using queue listen in laravel 10.</p>
+
+    <strong>Thanks & Regards.</strong>
+
+</body>
+
+</html>
